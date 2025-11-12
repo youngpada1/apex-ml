9a0ce5f474f7d33f1a6a38bb9e1b9a81a153f762
Flavia Ferreira <flavsferr@gmail.com>
2025-11-12 23:01:33 +0100

    Fix snowflake provider config and add tf variable bindings

--- infra/snowflake/variables.tf
+++ infra/snowflake/variables.tf
@@ -9,3 +9,13 @@ variable "etl_service_account_password" {
   sensitive   = true
   default     = ""
 }
+
+variable "snowflake_account" {
+  description = "Snowflake account locator or full name"
+  type        = string
+}
+
+variable "snowflake_user" {
+  description = "Snowflake username used for JTV authentication"
+  type.       = string
+}
