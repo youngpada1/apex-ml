592d6551e1e546eb9f9c5c73e4b1ee11ec0806a8
Flavia Ferreira <flavsferr@gmail.com>
2025-11-13 20:57:19 +0100

    fix(infra): remove unused snowflake_account and snowflake_user variables
    Provider reads these directly from SNOWFLAKE_ACCOUNT and SNOWFLAKE_USER environment variables

--- infra/snowflake/variables.tf
+++ infra/snowflake/variables.tf
@@ -9,13 +9,3 @@ variable "etl_service_account_password" {
   sensitive   = true
   default     = ""
 }
-
-variable "snowflake_account" {
-  description = "Snowflake account locator or full name"
-  type        = string
-}
-
-variable "snowflake_user" {
-  description = "Snowflake username used for JTV authentication"
-  type        = string
-}
